a07b8ca78a9dd5f7b5eb46718ac9adff240fbbe4
ggg199483 <447000429@qq.com>
2021-01-25 21:15:19 +0800

    还原

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
 import App from './App';
 import router from './router';
 import store from './store';
@@ -7,7 +6,6 @@ import './mock/index.js';  // 该项目所有请求使用mockjs模拟
 import './login.js' 
 import './ui.js' 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
 var vm=new Vue({
   el: '#app',
   router,
